a62e90b7a5143161e94473b7e8d1b3aba70ef4ea
Seokjin Yoon <sjy5386@gmail.com>
2019-12-31 13:00:10 +0900

    Add warning message

--- setup.cmd
+++ setup.cmd
@@ -2,6 +2,10 @@
 pushd %~dp0
 title autohansung
 echo autohansung
+echo.
+echo We will add a network profile.
+echo User information will be saved, so please set it on your personal PC only.
+echo Wi-Fi connection may be lost during setup.
 pause
 netsh wlan add profile filename="profile/hansung.xml" user=all
 if not %errorlevel% == 0 (
