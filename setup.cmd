bc7e4dccd32f81187b8e121c3b8b747e012f30c5
Seokjin Yoon <sjy5386@gmail.com>
2020-02-07 00:27:50 +0900

    Update script

--- setup.cmd
+++ setup.cmd
@@ -3,26 +3,33 @@ pushd %~dp0
 set session=%random%
 goto main
 
+rem Add network profiles.
 :add
 for %%a in ("profile/*.xml") do (
 	netsh wlan add profile filename="profile/%%a" user=all
 )
 goto :EOF
 
+rem Export network profiles.
+rem @param folder
 :export
-if not exist "%appdata%/AutoHansung/backup/%session%" (
-	mkdir "%appdata%/AutoHansung/backup/%session%"
+if not exist %1 (
+	mkdir %1
 )
 for %%a in ("profile/*.xml") do (
-	netsh wlan export profile name=%%~na folder="%appdata%/AutoHansung/backup/%session%"
+	netsh wlan export profile name=%%~na folder=%1
+)
+if not exist "%1/*.xml" (
+	rmdir %1
 )
 goto :EOF
 
 :main
+title AutoHansung
 echo AutoHansung
 echo.
 echo This script backs up your existing network profile before continuing.
-call :export
+call :export "%appdata%/AutoHansung/backup/%session%"
 echo.
 echo This script will add a network profile.
 echo User information will be saved, so please set it on your personal PC only.
