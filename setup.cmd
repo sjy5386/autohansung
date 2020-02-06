64c1f5313a2bf0ba21e8b8ca209d6b473aa5d018
Seokjin Yoon <sjy5386@gmail.com>
2020-02-07 00:04:40 +0900

    Add backup existing network profile

--- setup.cmd
+++ setup.cmd
@@ -1,21 +1,34 @@
 @echo off
 pushd %~dp0
+set session=%random%
 goto main
 
 :add
-echo We will add a network profile.
-echo User information will be saved, so please set it on your personal PC only.
-echo Wi-Fi connection may be lost during setup.
-pause
 for %%a in ("profile/*.xml") do (
 	netsh wlan add profile filename="profile/%%a" user=all
 )
-if not %errorlevel% == 0 (
-	pause
+goto :EOF
+
+:export
+if not exist "%appdata%/AutoHansung/backup/%session%" (
+	mkdir "%appdata%/AutoHansung/backup/%session%"
+)
+for %%a in ("profile/*.xml") do (
+	netsh wlan export profile name=%%~na folder="%appdata%/AutoHansung/backup/%session%"
 )
 goto :EOF
 
 :main
 echo AutoHansung
 echo.
+echo This script backs up your existing network profile before continuing.
+call :export
+echo.
+echo This script will add a network profile.
+echo User information will be saved, so please set it on your personal PC only.
+echo Wi-Fi connection may be lost during setup.
+pause
 call :add
+if not %errorlevel% == 0 (
+	pause
+)
