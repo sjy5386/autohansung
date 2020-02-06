9fa328c705cd088a3203e340499386d90da37c18
Seokjin Yoon <sjy5386@gmail.com>
2020-02-06 23:11:31 +0900

    Update script

--- setup.cmd
+++ setup.cmd
@@ -1,13 +1,21 @@
 @echo off
 pushd %~dp0
-title autohansung
-echo autohansung
-echo.
+goto main
+
+:add
 echo We will add a network profile.
 echo User information will be saved, so please set it on your personal PC only.
 echo Wi-Fi connection may be lost during setup.
 pause
-netsh wlan add profile filename="profile/hansung.xml" user=all
+for %%a in ("profile/*.xml") do (
+	netsh wlan add profile filename="profile/%%a" user=all
+)
 if not %errorlevel% == 0 (
 	pause
 )
+goto :EOF
+
+:main
+echo AutoHansung
+echo.
+call :add
